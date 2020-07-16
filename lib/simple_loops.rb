a99bf6188dd260deb914fed803484ee521135a9e
David Felix <david.felix02@hotmail.com>
2020-07-16 15:46:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,6 +19,10 @@ def output_array(array)
 end
 
 def return_string_array(array)
-   array.to_s
+  counter = 0
+  while array[counter] do
+    array[counter].to_s
+    counter+=1
+  end
    array
-end
+  end
